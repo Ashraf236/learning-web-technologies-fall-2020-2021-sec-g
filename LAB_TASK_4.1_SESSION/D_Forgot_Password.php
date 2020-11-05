4217feecb5999c5d4f70e461cf2539f8481b5897
Ashraf236 <you@example.com>
2020-11-06 00:00:17 +0600

    HTML

---
+++ LAB_TASK_4.1_SESSION/D_Forgot_Password.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORGOT PASSWORD</title>
+</head>
+<body>
+	<table border="1" width="100%">
+
+		<tr height="50px">
+			<td  width="100px">
+				<img src="logo.png" width="400px" height="200px">
+			</td>
+	             
+             <td colspan="2" align="right"> 
+	             <a href="A_Public_Home.php">Home</a>
+	             <a href="C_Login.php">Login</a>
+	             <a href="B_Registration.php">Registration</a>
+             </td>
+         </tr>
+         <tr height="100px">
+         	<td colspan="2" align="center">
+         		<form method="post" action="loginCheck.php">
+                  <fieldset>
+                     <legend>FORGOT PASSWORD</legend>
+                      <table width="300px">
+                       <tr>
+                         <td>Enter Email  :</td>
+                         <td><input type="email" name="email" ></td>
+                       </tr>
+                       <tr>  
+                          <td></td>
+                          <td><input type="submit" name="submit" value="Submit"></td>
+                       </tr>
+                     </table>
+                 </fieldset>
+                </form>
+         	</td>
+         </tr>
+         <tr>
+         	<td colspan="2" align="center">
+         		 <p>Copyright@2017</p>
+         	</td>
+         </tr>     
+    </table>
+</body>
+</html>
