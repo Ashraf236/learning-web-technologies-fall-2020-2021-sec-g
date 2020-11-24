f8b99739a16983b075e26688652e1076382bc368
Ashraf236 <you@example.com>
2020-11-24 11:28:54 +0600

    db connection complete

---
+++ FINAL_LAB_TASK_1.1_DB/view/home.php
@@ -0,0 +1,19 @@
+<?php
+	include('header.php');
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Home Page</title>
+</head>
+<body>
+	<h1>Welcome home, (need_to_add_user_name)</h1>
+	<a href="create.php">Create New User</a> |
+	<a href="profile.php">Profile</a> |
+	<a href="userlist.php">User List</a> |
+	<a href="../php/logout.php">logout</a>
+
+</body>
+</html>
