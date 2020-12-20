e57c0ad9d76086a347193f73a8a1bd3b3d694f7c
Ashraf236 <you@example.com>
2020-12-20 11:58:13 +0600

    comit at 11.52Am

--- FINAL_LAB_EXAM/service/deleteEmployee.php
+++ FINAL_LAB_EXAM/service/deleteEmployee.php
@@ -5,9 +5,9 @@
 	
 	function deleteUser($funame){
 		global $conn;
-		$sql = "delete from users where username ='$funame'";
+		$sql = "delete from users where username='$funame'";
 		mysqli_query($conn, $sql);
-		header('location: ../view/login.php?msg=user_deleted');
+		header('location: ../view/home.php?msg=user_deleted');
 	}
 
 ?>
